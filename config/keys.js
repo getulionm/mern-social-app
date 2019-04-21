461409820f0b0224b99e4e682c9566fe3ee0696f
Getulio Gasparotto Dadald <getulionm@gmail.com>
2019-04-21 18:55:42 +0100

    added routes for login, field validations and jwt auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://getulionm:123qwe@cluster0-jucsi.mongodb.net/test?retryWrites=true'
+  mongoURI:
+    "mongodb+srv://getulionm:123qwe@cluster0-jucsi.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,28 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
+//Body parser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
 //DB config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello!!'));
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
 
 //User routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
